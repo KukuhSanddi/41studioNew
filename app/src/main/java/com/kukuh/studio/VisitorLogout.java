264c1a06a5757c9b0788702af1701b2ab2837de4
KukuhSanddi <kukuhsanddi12@gmail.com>
2018-07-10 16:54:19 +0700

    Logo Fixed

--- app/src/main/java/com/kukuh/studio/VisitorLogout.java
+++ app/src/main/java/com/kukuh/studio/VisitorLogout.java
@@ -64,6 +64,7 @@ public class VisitorLogout extends AppCompatActivity {
             }
         });
 
+
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
